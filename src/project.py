a0966c388123b125de90ec31ad9a7ec7f199e310
alfreddo98 <67364721+alfreddo98@users.noreply.github.com>
2020-06-25 11:25:48 +0200

    Update project.py

--- src/project.py
+++ src/project.py
@@ -1,4 +1,6 @@
-from recorder import *
+
+# Import libraries, specially the library recorder qhere we have the methods we will use to record the song repetitively
+from BeatDetector.recorder import *
 from time import clock
 import time
 import sys
@@ -13,14 +15,24 @@ import argparse
 bpm_list = []
 prev_beat = clock()
 low_freq_avg_list = []
+    """
+@description: Method that predicts the genre of a song given the audio .WAV
+Inputs: Empty
+Outputs: The genre of the audio classified
+"""
 def predict():
-    song = 'audio.wav'
-    model_file='../TFG/custom_cnn_2d.h5'
+    song = '../data/samples/audio.wav'
+    model_file='../data/Model.h5'
     X = make_dataset_dl(song)
     model = tf.keras.models.load_model(model_file)
     preds = model.predict(X)
     votes = majority_voting(preds)
     return votes[0][0]
+    """
+@description: Method to convert a list of songs to a np array of melspectrograms this will be the most important method at the time of obtaining the data we will need to fit the model
+Inputs: Songs in the dataset, the number of fft that will be calculated, the number of samples between successive frames (hop_length)
+Outputs: An array of the melspectrogram values
+"""    
 def to_melspectrogram(songs, n_fft=1024, hop_length=256):
     # Transformation function
     melspec = lambda x: librosa.feature.melspectrogram(x, n_fft=n_fft,
@@ -31,7 +43,11 @@ def to_melspectrogram(songs, n_fft=1024, hop_length=256):
     # np.array([librosa.power_to_db(s, ref=np.max) for s in list(tsongs)])
     #print(np.array(list(tsongs)))
     return np.array(list(tsongs))
-
+    """
+@description: Method to split a song into multiple songs using overlapping windows this will help at the time of using differect length in time for the songs that will fit the model
+Inputs: The window length (always 0.5), the overlap length (always 0.5), the X signal and the y genre
+Outputs: Array of the new signal and the new array of genres (same as y but more9
+"""
 def splitsongs(X, overlap=0.5):
     # Empty lists to hold our results
     temp_X = []
@@ -50,7 +66,11 @@ def splitsongs(X, overlap=0.5):
         temp_X.append(s)
 
     return np.array(temp_X)
-
+    """
+@description: Method to call the other methods to split songs and obtaining the melspectrogram of the song recorded
+Inputs: The song file .WAV
+Outputs: The melspectrogram of the song
+"""
 def make_dataset_dl(song):
     # Convert to spectrograms and split into small windows
     signal, sr = librosa.load(song, sr=None)
@@ -63,13 +83,11 @@ def make_dataset_dl(song):
 
     return specs
 
-def get_genres(key, dict_genres):
-    # Transforming data to help on transformation
-    labels = []
-    tmp_genre = {v:k for k,v in dict_genres.items()}
-
-    return tmp_genre[key]
-
+   """
+@description: Method to return the genre given the classifier output
+Inputs: The scores of the classifier output and the directory of genres, it will be declared in the main
+Outputs: The genre clasiffied and the probability of each other
+"""
 def majority_voting(scores):
     preds = np.argmax(scores, axis = 1)
     values, counts = np.unique(preds, return_counts=True)
@@ -78,7 +96,11 @@ def majority_voting(scores):
     votes = {k: v for k, v in sorted(votes.items(), key=lambda item: item[1], reverse=True)}
     print(votes)
     return [(x, prob) for x, prob in votes.items()]
-
+    """
+@description: Method used to select figures and colors in which the lights will shine
+Inputs: The genre in an int form of the song
+Outputs: The figure of the leds ASCIIPIC and the r, g, b colors
+"""
 def seleccionar_colores(genero):
     if(genero == 0):
 #Genero Metal
@@ -221,6 +243,11 @@ def seleccionar_colores(genero):
         g = 0
         b = 0
     return r, g , b, ASCIIPIC
+    """
+@description: Method to make the leds shine
+Inputs: Time that the led will be on and the genre in an int form
+Outputs: Nothing, just the leds shining during that time
+"""
 def encender_luces(tiempo , genero):
     r, g , b, ASCIIPIC = seleccionar_colores(genero)
     for y in range(height):
@@ -233,10 +260,20 @@ def encender_luces(tiempo , genero):
                         unicorn.set_pixel(x,y,int(r),int(g),int(b))
     unicorn.show()
     time.sleep(tiempo)
+        """
+@description: Method to turn the leds off
+Inputs: ime that the leds will  be off
+Outputs: Nothing, just the leds being turned off during that time
+"""
 def apagar_luces(tiempo):
         unicorn.clear()
         unicorn.show()
-        time.sleep(tiempo)    
+        time.sleep(tiempo)
+    """
+@description: Method that will detect beats repetitively, it will make the leds turn on and off too
+Inputs: A variable to know whether the leds are on or off, the bpm_average computed adn the genre detected
+Outputs: Nothing just the program with the leds turning on and off with the corresponding figure
+"""    
 def detect_beats(encendido, bpm_avg, genero):
     if not input_recorder.has_new_audio: 
         return
@@ -320,7 +357,9 @@ def detect_beats(encendido, bpm_avg, genero):
         input_recorder.start()
 
 if __name__ == "__main__":
+    # Call the program that will record the audio in order to classiffy it
     os.system("python ../TFG/leeraudio1.py")
+    # Initialize leds matrix
     genero = predict()
     unicorn.set_layout(unicorn.HAT)
     unicorn.rotation(0)
